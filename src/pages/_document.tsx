4375b3f250edaa7f69f960517189f7f949d720ac
Furkan Kalaycıoğlu <furkan.kalaycioglu@anymile.io>
2023-05-21 18:57:20 +0300

    feat: meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,42 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+
+        <meta name="title" content="Spotify Friend Activity" />
+        <meta
+          name="description"
+          content="See what your friends are listening to on Spotify."
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://spotiwebfren.vercel.app/" />
+        <meta property="og:title" content="Spotify Friend Activity" />
+        <meta
+          property="og:description"
+          content="See what your friends are listening to on Spotify."
+        />
+        <meta
+          property="og:image"
+          content="https://spotiwebfren.vercel.app/vercel.svg"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://spotiwebfren.vercel.app/"
+        />
+        <meta property="twitter:title" content="Spotify Friend Activity" />
+        <meta
+          property="twitter:description"
+          content="See what your friends are listening to on Spotify."
+        />
+        <meta
+          property="twitter:image"
+          content="https://spotiwebfren.vercel.app/vercel.svg"
+        />
+      </Head>
 
       <body>
         <Main />
