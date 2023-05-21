21162e15c88c5e82b8e785b3d3abd7c73381cf80
Furkan Kalaycıoğlu <furkan.kalaycioglu@anymile.io>
2023-05-21 18:46:20 +0300

    feat: tailwind prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -92,15 +92,15 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="flex flex-col w-screen min-h-screen h-auto from-gray-900 to-gray-600 bg-gradient-to-r p-4 gap-6">
+    <main className="flex h-auto min-h-screen w-screen flex-col gap-6 bg-gradient-to-r from-gray-900 to-gray-600 p-4">
       <div className="flex flex-col gap-4">
-        <h1 className="text-4xl font-bold text-center text-white">
+        <h1 className="text-center text-4xl font-bold text-white">
           Spotify Friend Activity
         </h1>
         {sp_dc !== "" && (
-          <div className="flex flex-row gap-2 justify-center">
+          <div className="flex flex-row justify-center gap-2">
             <button
-              className="p-2 rounded-lg border  bg-transparent text-xs text-white font-bold hover:bg-green-500 h-10"
+              className="h-10 rounded-lg border  bg-transparent p-2 text-xs font-bold text-white hover:bg-green-500"
               onClick={() => {
                 localStorage.clear();
                 setSp_dc("");
@@ -110,7 +110,7 @@ export default function Home() {
               Clear local storage
             </button>
             <button
-              className="p-2 rounded-lg border  bg-transparent text-xs text-white font-bold hover:bg-green-500 h-10"
+              className="h-10 rounded-lg border  bg-transparent p-2 text-xs font-bold text-white hover:bg-green-500"
               onClick={() => {
                 getFriendActivity(sp_dc).then((data) => {
                   setFriendActivity(data);
@@ -123,34 +123,34 @@ export default function Home() {
         )}
       </div>
       {sp_dc === "" ? (
-        <div className="flex flex-col justify-center items-center gap-3">
+        <div className="flex flex-col items-center justify-center gap-3">
           <h3 className="text-2xl">No Key on local storage</h3>
           <input
             placeholder="SP_DC Key"
             type="text"
             onChange={(e) => setSp_dcTemp(e.target.value)}
-            className="px-3 py-2 text-gray-700 border rounded-lg focus:outline-none focus:shadow-outline"
+            className="focus:shadow-outline rounded-lg border px-3 py-2 text-gray-700 focus:outline-none"
           />
           <button
             onClick={() => {
               setKeyToLocalStorage(sp_dcTemp);
               setSp_dc(sp_dcTemp);
             }}
-            className="p-2 rounded-lg border  bg-transparent text-xs text-white font-bold hover:bg-green-500 h-10"
+            className="h-10 rounded-lg border  bg-transparent p-2 text-xs font-bold text-white hover:bg-green-500"
           >
             Save key to local storage
           </button>
         </div>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {friendActivity
             .sort((a: any, b: any) => b.timestamp - a.timestamp)
             .map((activity: any, index: number) => (
               <div
-                className="flex flex-row gap-3 py-2 px-3 items-center rounded-lg border-gray-500 border"
+                className="flex flex-row items-center gap-3 rounded-lg border border-gray-500 px-3 py-2"
                 key={index}
               >
-                <div className="w-12 h-12">
+                <div className="h-12 w-12">
                   {activity.user.imageUrl ? (
                     <Image
                       src={activity.user.imageUrl}
@@ -160,15 +160,15 @@ export default function Home() {
                       alt={activity.user.name + " profile picture"}
                     />
                   ) : (
-                    <div className="w-10 h-10 rounded-full bg-gray-500 flex justify-center items-center">
-                      <p className="text-white text-sm">
+                    <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-500">
+                      <p className="text-sm text-white">
                         {activity.user.name.charAt(0)}
                       </p>
                     </div>
                   )}
                 </div>
-                <div className="flex flex-col gap-1 w-full">
-                  <div className="flex flex-row gap-1 justify-between">
+                <div className="flex w-full flex-col gap-1">
+                  <div className="flex flex-row justify-between gap-1">
                     <a
                       href={activity.user.uri}
                       target="_blank"
@@ -177,7 +177,7 @@ export default function Home() {
                     >
                       {activity.user.name}
                     </a>
-                    <p className="text-gray-500 text-xs">
+                    <p className="text-xs text-gray-500">
                       {formatTimestamp(activity.timestamp)}
                     </p>
                   </div>
